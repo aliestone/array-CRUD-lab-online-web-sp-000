988d75f0987e27fa01034f1b573b7f3f89a948e7
Alie Stone <helloaliestone@gmail.com>
2020-09-07 23:32:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-create_an_array = [red, yellow, blue, green]
+create_an_array = ["red", "yellow", "blue", "green"]
 end
 
 def add_element_to_end_of_array(array, element)

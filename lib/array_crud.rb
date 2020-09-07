86616551d10afa74e044c67d49cab62f3a0a84e5
Alie Stone <helloaliestone@gmail.com>
2020-09-07 23:21:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
-end
+  []
+  end
 
 def create_an_array
-  
+  [red, yellow, blue, orange]
 end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  add_element_to_end_of_array << "alie"
+  end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end

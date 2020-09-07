5b422beaef4236b258d4c791b8859b7f05d43d46
Alie Stone <helloaliestone@gmail.com>
2020-09-07 23:53:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,14 @@
-def create_an_empty_array
+def create_an_empty_array (empty)
   []
 end
 
-def create_an_array
-  ["red", "yellow", "blue", "green"]
+def create_an_array (colors)
+  colors = ["red", "yellow", "blue", "green"]
 end
 
 def add_element_to_end_of_array
   ["Alie, Liz"]
-  add_element_to_end_of_array << "Quinn"
+add_element_to_end_of_array << "Quinn"
 end
 
 def add_element_to_start_of_array(array, element)

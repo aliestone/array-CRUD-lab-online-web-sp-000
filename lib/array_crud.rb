f5d140f6423546142832705165142eb125027d1e
Alie Stone <helloaliestone@gmail.com>
2020-09-07 23:43:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,6 @@ def create_an_array = ["red", "yellow", "blue", "green"]
 end
 
 def add_element_to_end_of_array = ["Alie", "Liz"]
-  add_element_to_end_of_array
   add_element_to_end_of_array << "Quinn"
 end
 
